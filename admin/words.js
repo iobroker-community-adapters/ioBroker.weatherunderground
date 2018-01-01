43090e1a42ef7874e961382c41a05b40e46977a4
Ingo Fischer <github@fischer-ka.de>
2018-01-02 00:57:53 +0100

    split admin translations into separate file

---
+++ admin/words.js
@@ -0,0 +1,11 @@
+systemDictionary = {
+    "location": { "en": "location", "de": "Ort", "nl": "Plaats" },
+    "station": { "en": "station", "de": "Messstation", "nl": "Weerstation" },
+    "apikey": { "en": "Apikey", "de": "Apikey", "nl": "Apikey" },
+    "forecast_periods_txt": { "en": "Forecast as Text for next 4 days and nights", "de": "Vorschau als Text für nächsten 4 Tage und Nächte", "nl": "Verwachting als de tekst voor de komende 4 dagen en nachten" },
+    "forecast_periods": { "en": "Forecast for next 4 days", "de": "Vorschau für nächsten 4 Tage", "nl": "Verwachting voor de komende 4 dagen" },
+    "forecast_hourly": { "en": "forecast hourly today", "de": "Vorschau heute stündlich", "nl": "Verwachting vandaag per uur" },
+    "current": { "en": "current data", "de": "Aktuelle Daten", "nl": "Huidige gegevens" },
+    "custom_icon_base_url": { "en": "Custom Icon-Base-URL", "de": "Eigene Icon-Basis-URL" },
+    "custom_icon_format": { "en": "Custom Icon-Format", "de": "Eigene Icon-Format" }
+};
